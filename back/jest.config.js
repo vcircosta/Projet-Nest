2387a95530e004f78b80e63c01d9c8a86f57894f
vcircosta <valentincircosta@gmail.com>
2025-02-06 17:07:10 +0100

    front

---
+++ back/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: './',
+    testMatch: ['**/*.e2e-spec.ts'],
+  };
+  
--- back/src/auth/jwt.strategy.ts
+++ back/src/auth/jwt.strategy.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt'; // Ces imports sont nécessaires pour le traitement JWT
-
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -10,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: process.env.JWT_SECRET || 'default_secret',
     });
   }
 
